f028013745f2139ecfb6b45d6b146cf71e134435
Francis Mbuyamba <mbuyambaf@gmail.com>
2022-09-08 01:43:06 -0400

    using matrix equation

--- testplace.py
+++ testplace.py
@@ -17,25 +17,23 @@ def check(*args):
         print(f'This member is stable! Resultant = {total}')
 
 
-# P
 # physical constants
 # material = 1  # mass(kg) per length
-#g = 9.81
 g = 9.81
 g1 = 9.81
 rho = 8000  # density if stainless steel AISI 316 Stainless Steel (kg/m3)
 
 # geometric propteries
 # AB
-l_AB = 1.28
+l_AB = 1.285
 vol_AB = 0.0022825499
+
 # rails (x2), shafts (x3)
-# additionPlatformMass = ((0.000288073749*2) + (0.00008123*3))*rho
-additionPlatformMass = (0)*rho
+additionPlatformMass = ((0.000288073749*2) + (0.00008123*3))*rho
+#additionPlatformMass = (0)*rho
 
 m_AB = vol_AB*rho + additionPlatformMass
 w_AB = m_AB*g
-
 
 # CD
 l_CD = 1.000
@@ -53,108 +51,62 @@ w_EB = m_EB*g
 
 ##############################################################################
 # inputs
-positions = 2
-
-#startAngle = 2.48*np.pi/180
-startAngle = 13.907*np.pi/180
-endAngle = 46.58*np.pi/180
-thetaList = np.linspace(endAngle, startAngle, num=positions)
+theta2 = 13.3*np.pi/180
+theta1 = 2.63365*np.pi/180
 
 gidMass = 0.350  # grams
 fullBin = 252
 FOS = 1
-PList = np.linspace(gidMass*g1, gidMass*fullBin*g1*FOS, num=positions)
+P = gidMass*fullBin*g1*FOS
 
 L1 = 0.400101  # connecting link length
-L2 = 0.148592 # fixing point from D
+L2 = 0.148592  # fixing point from D
 L3 = 0.127  # veritical displacement
 
 
 print("Results! \n")
-for theta, P in zip(thetaList, PList):
-    # secondary variables
-    L4 = (l_CD/2)-L2
-    alpha = np.arcsin((L3 + L2*np.sin(theta))/L1)
 
-    # calculation
-    # Link AB
-    Rbx = 0
-    Rby = (0.5*l_AB*(P+w_AB)-(l_CD*np.cos(theta)*(P+w_AB)))/(l_CD*np.cos(theta))
-    Rcx = 0
-    Rcy = w_AB + P - Rby
-    check(Rby, -P, -w_AB, Rcy, Rbx, Rcx)
-    print(Rby,Rcy)
+# secondary variables
+L4 = (l_CD/2)-L2
+alpha = np.arcsin((L3 + L2*np.sin(theta2))/L1)
 
-    # Solve for Rbx, Rby, Rcy 
-    a1 = np.matrix([[1,0,0],
-                    [0,1,1],
-                    [0,0,1]])
-    b1 = np.matrix([[0],
-                    [w_AB+P],
-                    [(0.5*l_AB*(P+w_AB))/(l_CD*np.cos(theta))]])
+a = np.matrix(
+    [
+        [1, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0],
+        [0, -1, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, np.cos(alpha)],
+        [0, 0, -1, 0, 0, 1, np.sin(alpha)],
+        [0, 0, l_CD*np.cos(theta1), 0, 0, 0, L2*(np.sin(alpha)
+                                                 * np.cos(theta2)+np.sin(theta2)*np.cos(alpha))],
+    ]
+)
+b = np.matrix(
+    [
+        [0],
+        [P+w_AB],
+        [0.5*l_AB*(P+w_AB)/(l_CD*np.cos(theta1))],
+        [w_EB],
+        [0],
+        [w_CD],
+        [-0.5*w_CD*l_CD*np.cos(theta1)]
+    ]
+)
 
-    x1 =  np.linalg.solve(a1, b1)
-    print('AB!')
-    print(x1)
-    check(x1[1,0], -P, -w_AB, x1[2,0], x1[0,0], Rcx)
+x = np.linalg.solve(a, b)
+print(x)
 
+requiredForcex = round(x[6, 0]*np.cos(alpha), 2)
+requiredForcey = round(x[6, 0]*np.sin(alpha), 2)
+angularPosition = round(theta2*180/np.pi, 2)
+massOfGIDs = round(P/g1, 2)
+numberGID = round(massOfGIDs/gidMass, 0)
 
-    # Link EB
-    Rfx = 0
-    Rfy = -2*Rby - w_EB
-    Rex = 0
-    Rey = Rfy + w_EB + Rby
-    check(-Rbx, -Rfx, Rex, -w_EB, Rey, -Rfy, -Rby)
-
-    # Solve for Rex, Rey, Rfx, Rfy 
-    a2 = np.matrix([[1,0,1,0],
-                    [0,1,0,-1],
-                    [np.tan(theta),-1,0,0],
-                    [0,0,0,1]])
-    b2= np.matrix([[Rbx],
-                    [Rby],
-                    [Rby+Rbx*np.tan(theta)],
-                    [Rfx]])
-
-    x2 =  np.linalg.solve(a2, b2)
-    print('EB!')
-    print(x2)
-    check(Rbx, x2[2,0], x2[0,0], -w_EB, x2[1,0], x2[3,0], x1[1,0])
-
-    # Link CD
-    F_hp = (0.5*l_CD*(2*Rcy - Rfy + w_CD))/(-0.5*l_CD*np.sin(alpha) +
-                                            (L4-0.5*l_CD)*np.tan(theta)*np.cos(alpha) + L4*np.sin(alpha))
-    Rdx = - F_hp * np.cos(alpha)
-    Rdy = Rcy - Rfy + w_CD - F_hp*np.sin(alpha)
-    check(Rdx, F_hp*np.cos(alpha), Rfx , -Rcy, Rfy,-w_CD, Rdy,F_hp*np.sin(alpha))
-
-    # Solve for Rdx, Rdy, F_hp 
-    a3 = np.matrix([[-1,0,-np.cos(alpha)],
-                    [0,1,np.sin(alpha)],
-                    [0.5*l_CD*np.sin(theta),0.5*l_CD*np.cos(theta),L4*(np.cos(theta)*np.sin(alpha) + np.cos(alpha)*np.sin(theta))]])
-    b3= np.matrix([[Rfx],
-                    [Rcy-Rfy+w_CD],
-                    [-0.5*l_CD*np.cos(theta)*Rcy]])
-
-    x3 =  np.linalg.solve(a3, b3)
-    print('CD!')
-    print(x3)
-    check(x3[0,0], x3[2,0]*np.cos(alpha), x2[2,0], x1[2,0], x2[2,0], -w_CD, x3[1,0], x3[2,0]*np.sin(alpha))
-
-    #results (outputs)
-    #requiredForce = round(F_hp*np.cos(alpha), 2)
-    requiredForce = round(x3[2,0]*np.cos(alpha), 2)
-    requiredForce2 = round(x3[2,0]*np.sin(alpha), 2)
-    angularPosition = round(theta*180/np.pi, 2)
-    massOfGIDs = round(P/g1, 2)
-    numberGID = round(massOfGIDs/gidMass, 0)
-
-
-    print(
-        f'Required pump force: {requiredForce} at angle {angularPosition} degrees with mass {massOfGIDs} kg')
-    #requiredForce2 = round(F_hp*np.sin(alpha), 2)
-    print(requiredForce)
-    print(requiredForce2)
-    # print(alpha*180/np.pi)
-    if abs(requiredForce) <= 6000:
-        print('This is the ideal position!!!!!!!!!!!!!!!!!!')
+print(
+    f'Required pump force: {requiredForcex} at angle {angularPosition} degrees with mass {massOfGIDs} kg')
+print(requiredForcex)
+print(requiredForcey)
+# print(alpha*180/np.pi)
+if abs(requiredForcex) <= 1000*g1/FOS:
+    print('This Meets your Saferty Factor')
